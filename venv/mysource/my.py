3899c472e151180ba2c30f9aaa5f5eedb888c7a6
downslj <downslj@gmail.com>
2018-07-01 19:34:52 -0500

    commit insert

--- venv/mysource/my.py
+++ venv/mysource/my.py
@@ -35,6 +35,19 @@ conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
 conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
       VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )");
 
+conn.execute("INSERT INTO COMPANY2 (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (1, 'Paul', 32, 'California', 20000.00 )");
+
+conn.execute("INSERT INTO COMPANY2 (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (2, 'Allen', 25, 'Texas', 15000.00 )");
+
+conn.execute("INSERT INTO COMPANY2 (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )");
+
+conn.execute("INSERT INTO COMPANY2 (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )");
+
+
 conn.commit()
 conn.close()
 print("Records created success")
